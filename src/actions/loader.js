244162a8f61aae125cb5fe84686f7e293e15f56e
Fabian Terh <fabianterh@gmail.com>
2018-06-15 23:57:58 +0800

    Type check Redux actions

--- src/actions/loader.js
+++ src/actions/loader.js
@@ -5,20 +5,39 @@ export const UPDATE_LOADER_LOADED = 'UPDATE_LOADER_LOADED';
 export const UPDATE_LOADER_STATUS = 'UPDATE_LOADER_STATUS';
 export const UPDATE_LOADER_ERROR = 'UPDATE_LOADER_ERROR';
 
+export type $loaderLoadedAction = {
+  type: 'UPDATE_LOADER_LOADED',
+  loaded: boolean
+};
+
+export type $loaderStatusAction = {
+  type: 'UPDATE_LOADER_STATUS',
+  status: ?string
+};
+export type $loaderErrorAction = {
+  type: 'UPDATE_LOADER_ERROR',
+  error: boolean
+};
+
+export type $loaderActions =
+ | $loaderLoadedAction
+ | $loaderStatusAction
+ | $loaderErrorAction;
+
 // Action creators
-export function updateLoaderLoaded(loaded: boolean) {
+export function updateLoaderLoaded(loaded: boolean): $loaderLoadedAction {
   return {
     type: UPDATE_LOADER_LOADED,
     loaded: loaded
   };
 };
-export function updateLoaderStatus(status: ?string) {
+export function updateLoaderStatus(status: ?string): $loaderStatusAction {
   return {
     type: UPDATE_LOADER_STATUS,
     status: status
   }
 };
-export function updateLoaderError(error: boolean) {
+export function updateLoaderError(error: boolean): $loaderErrorAction {
   return {
     type: UPDATE_LOADER_ERROR,
     error: error
--- src/actions/payload.js
+++ src/actions/payload.js
@@ -1,15 +1,18 @@
 // @flow
 
-export type $payload = {
-  name: string,
-  data: {}
-}
+import type $payload from './../types/payload';
 
 // Action types
 export const UPDATE_PAYLOAD = 'UPDATE_PAYLOAD';
 
+export type $payloadAction = {
+  type: 'UPDATE_PAYLOAD',
+  name: string,
+  data: {}
+};
+
 // Action creators
-export function updatePayload(payload: $payload) {
+export function updatePayload(payload: $payload): $payloadAction {
   return {
     type: UPDATE_PAYLOAD,
     name: payload.name,
