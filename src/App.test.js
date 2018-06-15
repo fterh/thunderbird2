950a9e6216e7a0a0d3ca86d4de33e01e96fa2fb8
Fabian Terh <fabianterh@gmail.com>
2018-06-15 21:56:26 +0800

    Fix app.test.js failing

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
