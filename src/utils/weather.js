067da0c02d23a7be44d7ef93d27164c31088e5e9
Fabian Terh <fabianterh@gmail.com>
2018-06-16 22:15:13 +0800

    Fix missing // @flow declaration

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -4,7 +4,7 @@ This wrapper function imports and calls the individual functions for fetching
 specific weather data, wraps their promises using Promise.all, and returns it.
 */
 
-import type $payload from './../types/payload';
+import type { $payload } from './../types/payload';
 import temperature from './weather/temperature';
 import uvIndex from './weather/uvIndex';
 import humidity from './weather/humidity';
